f888816ed4cec68a3d66fbb3dad57e471af048a4
= <=>
2026-01-01 14:08:08 -0600

    feat: implement translations for pages

--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { Lang, LanguageService } from 'src/app/services/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
   styleUrls: ['./create.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+  ],
 })
 export class CreatePage implements OnInit {
+  constructor(public lang: LanguageService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLangChange(v: string) {
+    this.lang.set(v as Lang);
   }
-
 }
--- src/app/pages/creations/creations.page.ts
+++ src/app/pages/creations/creations.page.ts
@@ -2,19 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { Lang, LanguageService } from 'src/app/services/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-creations',
   templateUrl: './creations.page.html',
   styleUrls: ['./creations.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+  ],
 })
 export class CreationsPage implements OnInit {
+  constructor(public lang: LanguageService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onLangChange(v: string) {
+    this.lang.set(v as Lang);
   }
-
 }
